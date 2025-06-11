9ae852dc39261451f720ab70820bc728aa25a8cc
Hieu Nguyen <hieunguyen23@augustana.edu>
2025-06-10 21:13:46 -0500

    Added comments to the database class

--- blog/database.py
+++ blog/database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db' #Connect to the blog database
 
 
-
+######Create a connection to the SQLite database######
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 Base = declarative_base()
--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from .database import Base
 from sqlalchemy import Column, String, Integer
 class Blog(Base):
-    __tablename__ = "blogs-db"
+    __tablename__ = "blogs-db" #Create the SQLite table schemas.
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
